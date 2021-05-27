7b72afc3250c9af4c46769f7986965c8c6d3849d
jwhan324 <79911221+jwhan324@users.noreply.github.com>
2021-05-27 14:40:40 +0900

    course list
    (cherry picked from commit b5be6c142edd494479ab54f593529333277ddeb6)
    
    # Conflicts:
    #	routers/programs.js
    #	src/views/debate-course.ejs
    #	src/views/elementary-course.ejs
    #	src/views/middle-high-school-course.ejs
    #	src/views/test-preparation.ejs

---
+++ public/js/sub/program.js
@@ -0,0 +1,88 @@
+const courseFilters = [];
+let courseSort = 'newest';
+const courseFilter = (option, value, on) => {
+  const items = $('.class_box_type_01--item');
+  if(on) {
+    courseFilters.push({option, value});
+  } else {
+    courseFilters.splice(courseFilters.findIndex(filter => filter.value === option && filter.value === value));
+  }
+}
+
+const getCourses = (type, limit) => {
+  let typeCourses = courses;
+  switch(type) {
+    case 'elementary':
+      typeCourses = typeCourses.filter(course => course.program === 'Elementary')
+      break;
+    case 'middlehigh':
+      typeCourses = typeCourses.filter(course => course.program === 'Middle & High School')
+      break;
+    case 'debate':
+      typeCourses = typeCourses.filter(course => course.program === 'Debate')
+      break;
+    case 'test':
+      typeCourses = typeCourses.filter(course => course.program === 'Test Prep')
+      break;
+  }
+
+  let items = [];
+  if(courseFilters.length > 0) {
+    for(const course of typeCourses) {
+      for(const filter of courseFilters) {
+        if(course[filter.option] === filter.value) {
+          items.push(course);
+          break;
+        }
+      }
+    }
+  } else {
+    items = typeCourses;
+  }
+
+  items = items.sort(course => {
+  });
+
+  if(limit > 0) {
+    items = items.splice(0, limit);
+  }
+
+  return items;
+}
+
+const bindCourse = (course) => {
+  const template = $(courseTemplate);
+  template.find('.class_box_type_01--img a').attr('href', `/programs/${course.courseId}`);
+  template.find('.class_box_type_01--img img').attr('src', course.thumbnail).attr('alt', `${course?.name} thumbnail`);
+  template.find('.age').html(`Age ${course.age}`);
+  template.find('.times').html(`${course.runtime}`);
+  template.find('.title_r_03').html(course.name);
+  template.find('.class_type').html(course.type);
+  return template;
+}
+
+const renderCourse = (type, limit = 0) => {
+  $('.courses_type_box .class_box_type_01').html('');
+  getCourses(type, limit).map(course => {
+    $('.courses_type_box .class_box_type_01').append(bindCourse(course));
+  });
+}
+
+const renderCourses = (limit = 0) => {
+  $('.courses_type_box.elementary .class_box_type_01').html('');
+  getCourses('elementary', limit).map(course => {
+    $('.courses_type_box.elementary .class_box_type_01').append(bindCourse(course));
+  });
+  $('.courses_type_box.middlehigh .class_box_type_01').html('');
+  getCourses('middlehigh', limit).map(course => {
+    $('.courses_type_box.middlehigh .class_box_type_01').append(bindCourse(course));
+  });
+  $('.courses_type_box.debate .class_box_type_01').html('');
+  getCourses('debate', limit).map(course => {
+    $('.courses_type_box.debate .class_box_type_01').append(bindCourse(course));
+  });
+  $('.courses_type_box.test .class_box_type_01').html('');
+  getCourses('test', limit).map(course => {
+    $('.courses_type_box.test .class_box_type_01').append(bindCourse(course));
+  });
+}
--- routers/programs.js
+++ routers/programs.js
@@ -1,29 +1,87 @@
 const express = require('express');
 const router = express.Router();
+const courses = require('../src/data/courses')
+const options = (courses) => [
+  {
+    key: 'location',
+    name: 'Location',
+    filters: [
+      { idx:1, name: 'Hanoi - Cau Giay', value: 'Hanoi - Cau Giay', count: () => courses.filter(course => course.location === 'Hanoi - Cau Giay').length },
+      { idx:2, name: 'Hanoi - Long Bien', value: 'Hanoi - Long Bien', count: () => courses.filter(course => course.location === 'Hanoi - Long Bien').length },
+      { idx:3, name: 'Bangkok', value: 'Bangkok', count: () => courses.filter(course => course.location === 'Bangkok').length },
+    ]
+  },
+  {
+    key: 'type',
+    name: 'Course Type',
+    filters: [
+      { idx:1, name: 'Online', value: 'Online', count: () => courses.filter(course => course.type === 'Online').length },
+      { idx:2, name: 'Offline', value: 'Offline', count: () => courses.filter(course => course.type === 'Offline').length },
+      { idx:3, name: 'Hybrid', value: 'Hybrid', count: () => courses.filter(course => course.type === 'Hybrid').length },
+    ]
+  },
+  {
+    key: 'age',
+    name: 'Age',
+    filters: [
+      { idx:1, name: '5 - 10', value: '5-10', count: () => courses.filter(course => course.age === '5-10').length },
+      { idx:2, name: '11 - 15', value: '11-15', count: () => courses.filter(course => course.age === '11-15').length },
+      { idx:3, name: '16 - 18', value: '16-18', count: () => courses.filter(course => course.age === '16-18').length },
+    ]
+  },
+  // {
+  //   key: 'program',
+  //   name: 'Program',
+  //   filters: [
+  //     { idx:1, name: 'Test Prep', value: 'Test Prep', count: () => courses.filter(course => course.program === 'Test Prep').length },
+  //     { idx:2, name: 'Elementary', value: 'Elementary', count: () => courses.filter(course => course.program === 'Elementary').length },
+  //     { idx:3, name: 'Middle & High School', value: 'Middle & High School', count: () => courses.filter(course => course.program === 'Middle & High School').length },
+  //     { idx:4, name: 'Debate', value: 'Debate', count: () => courses.filter(course => course.program === 'Debate').length },
+  //   ]
+  // }
+]
 
 router.get('/', function (req, res) {
-  const courses = require('../src/data/courses')
-  res.render('programs', { courses });
+  const elementaryCourses = courses.filter(course => course.program === 'Elementary').splice(0, 3);
+  const middleCourses = courses.filter(course => course.program === 'Middle & High School').splice(0, 3);
+  const debateCourses = courses.filter(course => course.program === 'Debate').splice(0, 3);
+  const testCourses = courses.filter(course => course.program === 'Test Prep').splice(0, 3);
+  res.render('programs', { options: options(courses), courses, elementaryCourses, middleCourses, testCourses, debateCourses });
 })
 
 router.get('/elementary-courses', function (req, res) {
-  res.render('elementary-course');
-  // res.send('Courses > Elementary courses')
-})
+  const filterCourse = courses.filter(course => course.program === 'Elementary');
+  const elementaryTeachersData = require('../src/data/programs/teachers/elementary.json');
+  res.render('elementary-course', { options: options(filterCourse), courses: filterCourse, teachers: elementaryTeachersData });
+});
 
 router.get('/middle-high-school-courses', function (req, res) {
-  res.render('middle-high-school-course');
-  // res.send('Courses > Middle & Highschool courses')
-})
+  const filterCourse = courses.filter(course => course.program === 'Middle & High School');
+  const middleHighTeachersData = require('../src/data/programs/teachers/middle-high.json');
+  res.render('middle-high-school-course', { options: options(filterCourse), courses: filterCourse, teachers: middleHighTeachersData });
+});
 
 router.get('/debate', function (req, res) {
-  res.render('debate-course')
-})
+  const filterCourse = courses.filter(course => course.program === 'Debate');
+  console.log(filterCourse)
+  const debateTeachersData = require('../src/data/programs/teachers/debate.json');
+  res.render('debate-course', { options: options(filterCourse), courses: filterCourse, teachers: debateTeachersData });
+});
 
 router.get('/test-preparation', function (req, res) {
-  // res.send('Courses > Test-preparation')
-  res.render('test-preparation')
-})
+  const filterCourse = courses.filter(course => course.program === 'Test Prep');
+  const testPrepTeachersData = require('../src/data/programs/teachers/test-prep.json');
+  res.render('test-preparation', { options: options(filterCourse), courses: filterCourse, teachers: testPrepTeachersData });
+});
 
+router.get('/:courseId', function (req, res) {
+  const { courseId } = req.params;
+  const course = courses.filter(course => course.courseId == courseId)[0];
+  let renderPath = 'course-detail';
+  if(course.type === 'Offline') {
+    renderPath = 'course-detail-offline';
+  }
+  res.render(renderPath, { course })
+});
 
 module.exports = router
