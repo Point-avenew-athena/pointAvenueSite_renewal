718668083d3025bbfceccd8c56db6a7b5c27f03b
allenYeo77 <>
2021-05-27 10:54:08 +0900

    case-studies.json 경로 변경

--- routers/admissions-consulting.js
+++ routers/admissions-consulting.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const studiesData = require('../src/data/admissions-consulting/case-studies.json');
+const studiesData = require('../data/point-avenue/admissions-consulting/case-studies.json')
 
 router.get('/', function (req, res) {
   res.render('admissions-consulting',{ studies : studiesData })
