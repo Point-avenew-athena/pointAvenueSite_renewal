f0c6182a181c83ae9196710cbf0c5f438a2551e0
prod deploy server <deploy@paathena.com>
2021-05-28 11:12:14 +0900

    skip github action

--- deploy/prod/deploy.sh
+++ deploy/prod/deploy.sh
@@ -1,7 +1,7 @@
 set -e 
 
 ecrHost='367893350766.dkr.ecr.ap-southeast-1.amazonaws.com'
-imageTag='0.0.16'
+imageTag='0.0.17'
 localImage="pasite:${imageTag}"
 remoteImage="${ecrHost}/${localImage}"
 
