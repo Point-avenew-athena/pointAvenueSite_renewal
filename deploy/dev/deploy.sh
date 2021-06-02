c0d7329e4e25e9e88bbd5a6e047a9239cd5434b8
jwhan324 <79911221+jwhan324@users.noreply.github.com>
2021-06-02 11:59:19 +0900

    dev env

---
+++ deploy/dev/deploy.sh
@@ -0,0 +1,15 @@
+set -e 
+
+ecrHost='367893350766.dkr.ecr.ap-northeast-2.amazonaws.com'
+imageTag='0.0.0'
+localImage="pasite:${imageTag}"
+remoteImage="${ecrHost}/${localImage}"
+
+docker build -t ${localImage} --build-arg ENV=development -f deploy/dev/Dockerfile .
+docker tag ${localImage} ${remoteImage}
+
+aws ecr get-login-password --region ap-northeast-2 | docker login --username AWS --password-stdin "${ecrHost}"
+docker push ${remoteImage}
+
+kubectl apply -f deploy/dev/deployment.yaml
+kubectl apply -f deploy/dev/service.yaml
