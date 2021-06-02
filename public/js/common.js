ba94c7467dd80731d2472230aab2772522acf757
allenYeo77 <>
2021-06-02 11:13:18 +0900

    change

--- public/js/common.js
+++ public/js/common.js
@@ -1,18 +1,15 @@
 // let experience = document.querySelector('.mentor_info_box .ul_type_02');
 
-window.onload = function (){
-  if(window.location.href.includes('localhost') || window.location.href.includes('127.0.0.1')){
-      console.log('Local Development')
-  } else {
-    location.protocol == 'http:' && (location.href = location.href.replace('http:', 'https:'));
-  }
-}
 $(document).ready(function () {
   // http -> https
   // if (location.protocol == 'http:') {
   //   location.href = location.href.replace('http:', 'https:');
   // }
-
+    if(window.location.href.includes('localhost') || window.location.href.includes('127.0.0.1')){
+        console.log('Local Development')
+    } else {
+      location.protocol == 'http:' && (location.href = location.href.replace('http:', 'https:'));
+    }
   /* ========= common :: start=============*/
   $('.contact_pop_btn').click(function () {
     $('.contact_popup').show();
