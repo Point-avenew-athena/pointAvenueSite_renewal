8e4d96631e256a39f66d0b4c9775790ea8ca971a
Hyewon <hyewon.lee@paathena.com>
2021-06-04 11:06:16 +0900

    - 팝업 esc키로 닫을 수 있게 스크립트 추가
    - admission-consulting 페이지 내 팝업 dim 처리 안된 부분 수정

--- public/js/sub/admission-consulting/admission-consulting.js
+++ public/js/sub/admission-consulting/admission-consulting.js
@@ -91,11 +91,10 @@ $(function(){
 		}
 	});
 
-	/*mentor 클릭시, mentor 상세 팝업 노출*/
+	/* case-studies 클릭시,case-studies 상세 팝업 노출*/
 	$('.admission_consulting .class_box_type_01 a').click(function(){
 		$('.case_studies_popup').show();
 		changeStudiesData(studies, document.querySelector('.case_studies_info_box .ul_type_02'), this);
-		toggleScroll();
 		$('.dim').show();
 	});
 });
