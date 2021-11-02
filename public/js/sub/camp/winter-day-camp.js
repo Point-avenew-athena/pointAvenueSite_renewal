97f14140a29aa678db14f0f59c8a412856a7d9de
Hyewon <hyewon.lee@paathena.com>
2021-11-02 18:18:48 +0900

    winter camp 영문버전 수정

--- public/js/sub/camp/winter-day-camp.js
+++ public/js/sub/camp/winter-day-camp.js
@@ -28,22 +28,18 @@ $(function(){
 	
 	/*=================== case_studies_swiper ==============*/
 	var case_studies_swiper = new Swiper('.testimonials_wrap .swiper-container', {
-		slidesPerView: 4,
-		spaceBetween: 20,
+		slidesPerView: 1, 
+		
+		effect:'fade',
+		pagination: {
+			el: '.swiper-pagination',
+			clickable: true
+		},
 		navigation: {
 			nextEl: '.swiper-button-next',
 			prevEl: '.swiper-button-prev',
-		},
-		breakpoints: {
-			1000: {  // 화면의 넓이가 1000px 이하일 때 
-				slidesPerView: 2,
-			},
-			768: {  // 화면의 넓이가 768px 이하일 때 
-				slidesPerView: 1,
-				spaceBetween: 10,
-			}
 		}
-	});
+    });
 
 	/* title 클릭시, 활성화 */
 	$('.day_camp .camp_info_wrap .text_article .title_r_02').click(function(){
