0e17f8374e7bc31c1075387a0277880dbcaf4ede
Hyeon Jong <hyeonjong.an@paathena.com>
2022-03-15 11:13:33 +0900

    send ses

--- server.js
+++ server.js
@@ -51,3 +51,59 @@ app.use("/vn/admissions-consulting", admissionsConsultingVn);
 app.listen(PORT, () => {
   console.log(`Server is connected to ${PORT}`);
 });
+
+// Load the AWS SDK for Node.js
+var AWS = require('aws-sdk');
+
+AWS.config.update({
+  // Only to send mail, access key
+  accessKeyId: 'AKIAVLKBHBFXGJO2IBOP',
+  secretAccessKey: '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',
+  region: 'ap-southeast-1'
+});
+
+app.post('/email/summer-boarding-camp', function (req, res) {
+  var params = {
+    Destination: { /* required */
+      CcAddresses: [
+        /* more items */
+      ],
+      ToAddresses: [
+        req.body.email,
+        /* more items */        
+      ]
+    },
+    Message: { /* required */
+      Body: { /* required */
+        Html: {
+         Charset: "UTF-8",
+         Data: "HTML_FORMAT_BODY"
+        },
+        Text: {
+         Charset: "UTF-8",
+         Data: "TEXT_FORMAT_BODY"
+        }
+       },
+       Subject: {
+        Charset: 'UTF-8',
+        Data: 'Test email'
+       }
+      },
+    Source: 'contact@pointavenue.com', /* required */
+    ReplyToAddresses: [
+      /* more items */
+    ],
+  };
+
+  // Create the promise and SES service object
+var sendPromise = new AWS.SES({apiVersion: '2010-12-01'}).sendEmail(params).promise();
+
+// Handle promise's fulfilled/rejected states
+sendPromise.then(
+  function(data) {    
+    res.send('ok')
+  }).catch(
+    function(err) {
+      res.send(err)
+  });
+})
