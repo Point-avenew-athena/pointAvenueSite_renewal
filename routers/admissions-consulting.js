7683faddde08e0999ee7d3f1b053ef3d49269748
le trung <le.trung@truenorth.edu.vn>
2022-04-08 16:19:54 +0700

    change page AC

--- routers/admissions-consulting.js
+++ routers/admissions-consulting.js
@@ -2,9 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', function (req, res) {
+  const news = require("../data/point-avenue/news/news.json");
   const studies = require('../data/admissions-consulting/case-studies.json');
   const mentors = require('../data/admissions-consulting/mentors.json');
-  res.render('admissions-consulting', { studies, mentors });
+  res.render('admissions-consulting', { studies, mentors, news });
 });
 
 
