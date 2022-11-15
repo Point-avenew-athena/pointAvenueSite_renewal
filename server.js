0bb90b89ade641dbbdc11f48d98f416856d2012f
jwhan-everyon <jw.han@everyoncorp.com>
2022-11-15 12:24:57 +0900

    add health check page

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const bodyParser = require('body-parser')
-app.use(bodyParser.urlencoded())
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded());
 // const favicon = require('serve-favicon');
 //* PORT
 const PORT = process.env.PORT || 7070;
@@ -59,244 +59,267 @@ app.use("/vn/admissions-consulting", admissionsConsultingVn);
 app.use("/blog", blog);
 app.use("/vn/blog", blogVn);
 
+app.use("/health-check", (req, res) => {
+  res.send("success");
+});
 
 app.listen(PORT, () => {
   console.log(`Server is connected to ${PORT}`);
 });
 
 // Load the AWS SDK for Node.js
-var AWS = require('aws-sdk');
+var AWS = require("aws-sdk");
 
 AWS.config.update({
   // Only to send mail, access key
-  accessKeyId: 'AKIAVLKBHBFXGJO2IBOP',
-  secretAccessKey: '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',
-  region: 'ap-southeast-1'
+  accessKeyId: "AKIAVLKBHBFXGJO2IBOP",
+  secretAccessKey: "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",
+  region: "ap-southeast-1",
 });
 
-app.post('/camp/summer-boarding-camp', function (req, res) {
+app.post("/camp/summer-boarding-camp", function (req, res) {
   if (req.body.email) {
     var params = {
-      Destination: { /* required */
+      Destination: {
+        /* required */
         CcAddresses: [
           /* more items */
         ],
         ToAddresses: [
-          "contact@pointavenue.com"
+          "contact@pointavenue.com",
           // req.body.email,
-          /* more items */        
-        ]
+          /* more items */
+        ],
       },
-      Message: { /* required */
-        Body: { /* required */
+      Message: {
+        /* required */
+        Body: {
+          /* required */
           Html: {
-           Charset: "UTF-8",
-           Data: `
+            Charset: "UTF-8",
+            Data: `
             <p><b>First Name: </b> ${req.body.firstName}</p>
             <p><b>Last Name: </b> ${req.body.lastName}</p>
             <p><b>Email: </b> ${req.body.email}</p>
             <p><b>Child Name: </b> ${req.body.childName}</p>
             <p><b>Phone Number: </b> ${req.body.phoneNumber}</p>
             <p><b>Student's Grade & School: </b> ${req.body.grade}</p>
-          `
+          `,
           },
           Text: {
-           Charset: "UTF-8",
-           Data: "TEXT_FORMAT_BODY"
-          }
-         },
-         Subject: {
-          Charset: 'UTF-8',
-          Data: 'Register Summer Boarding Camp'
-         }
+            Charset: "UTF-8",
+            Data: "TEXT_FORMAT_BODY",
+          },
         },
-      Source: 'contact@pointavenue.com', /* required */
+        Subject: {
+          Charset: "UTF-8",
+          Data: "Register Summer Boarding Camp",
+        },
+      },
+      Source: "contact@pointavenue.com" /* required */,
       ReplyToAddresses: [
         /* more items */
       ],
     };
-  
+
     // Create the promise and SES service object
-    var sendPromise = new AWS.SES({apiVersion: '2010-12-01'}).sendEmail(params).promise();
-  
-  // Handle promise's fulfilled/rejected states
-    sendPromise.then(
-      function(data) {  
-        res.status(204).send()  
-      }).catch(
-        function(err) {
-          res.send(err)
+    var sendPromise = new AWS.SES({ apiVersion: "2010-12-01" })
+      .sendEmail(params)
+      .promise();
+
+    // Handle promise's fulfilled/rejected states
+    sendPromise
+      .then(function (data) {
+        res.status(204).send();
+      })
+      .catch(function (err) {
+        res.send(err);
       });
   }
-})
+});
 
-app.post('/camp/summer-day-camp', function (req, res) {
+app.post("/camp/summer-day-camp", function (req, res) {
   if (req.body.email) {
     var params = {
-      Destination: { /* required */
+      Destination: {
+        /* required */
         CcAddresses: [
           /* more items */
         ],
         ToAddresses: [
-          "contact@pointavenue.com"
+          "contact@pointavenue.com",
           // "trung.le@truenorth.edu.vn"
           // req.body.email,
-          /* more items */        
-        ]
+          /* more items */
+        ],
       },
-      Message: { /* required */
-        Body: { /* required */
+      Message: {
+        /* required */
+        Body: {
+          /* required */
           Html: {
-           Charset: "UTF-8",
-           Data: `
+            Charset: "UTF-8",
+            Data: `
             <p><b>First Name: </b> ${req.body.firstName}</p>
             <p><b>Last Name: </b> ${req.body.lastName}</p>
             <p><b>Email: </b> ${req.body.email}</p>
             <p><b>Child Name: </b> ${req.body.childName}</p>
             <p><b>Phone Number: </b> ${req.body.phoneNumber}</p>
             <p><b>Student's Grade & School: </b> ${req.body.grade}</p>
-          `
+          `,
           },
           Text: {
-           Charset: "UTF-8",
-           Data: "TEXT_FORMAT_BODY"
-          }
-         },
-         Subject: {
-          Charset: 'UTF-8',
-          Data: 'Register Summer Day Camp'
-         }
+            Charset: "UTF-8",
+            Data: "TEXT_FORMAT_BODY",
+          },
         },
-      Source: 'contact@pointavenue.com', /* required */
+        Subject: {
+          Charset: "UTF-8",
+          Data: "Register Summer Day Camp",
+        },
+      },
+      Source: "contact@pointavenue.com" /* required */,
       ReplyToAddresses: [
         /* more items */
       ],
     };
-  
+
     // Create the promise and SES service object
-    var sendPromise = new AWS.SES({apiVersion: '2010-12-01'}).sendEmail(params).promise();
-  
-  // Handle promise's fulfilled/rejected states
-    sendPromise.then(
-      function(data) {  
-        res.status(204).send()  
-      }).catch(
-        function(err) {
-          res.send(err)
+    var sendPromise = new AWS.SES({ apiVersion: "2010-12-01" })
+      .sendEmail(params)
+      .promise();
+
+    // Handle promise's fulfilled/rejected states
+    sendPromise
+      .then(function (data) {
+        res.status(204).send();
+      })
+      .catch(function (err) {
+        res.send(err);
       });
   }
-})
+});
 
-app.post('/camp/winter-boarding-camp', function (req, res) {
+app.post("/camp/winter-boarding-camp", function (req, res) {
   if (req.body.email) {
     var params = {
-      Destination: { /* required */
+      Destination: {
+        /* required */
         CcAddresses: [
           /* more items */
         ],
         ToAddresses: [
-          "contact@pointavenue.com"
+          "contact@pointavenue.com",
           // "trung.le@truenorth.edu.vn"
           // req.body.email,
-          /* more items */        
-        ]
+          /* more items */
+        ],
       },
-      Message: { /* required */
-        Body: { /* required */
+      Message: {
+        /* required */
+        Body: {
+          /* required */
           Html: {
-           Charset: "UTF-8",
-           Data: `
+            Charset: "UTF-8",
+            Data: `
             <p><b>First Name: </b> ${req.body.firstName}</p>
             <p><b>Last Name: </b> ${req.body.lastName}</p>
             <p><b>Email: </b> ${req.body.email}</p>
             <p><b>Child Name: </b> ${req.body.childName}</p>
             <p><b>Phone Number: </b> ${req.body.phoneNumber}</p>
             <p><b>Student's Grade & School: </b> ${req.body.grade}</p>
-          `
+          `,
           },
           Text: {
-           Charset: "UTF-8",
-           Data: "TEXT_FORMAT_BODY"
-          }
-         },
-         Subject: {
-          Charset: 'UTF-8',
-          Data: 'Register Winter Boarding Camp'
-         }
+            Charset: "UTF-8",
+            Data: "TEXT_FORMAT_BODY",
+          },
         },
-      Source: 'contact@pointavenue.com', /* required */
+        Subject: {
+          Charset: "UTF-8",
+          Data: "Register Winter Boarding Camp",
+        },
+      },
+      Source: "contact@pointavenue.com" /* required */,
       ReplyToAddresses: [
         /* more items */
       ],
     };
-  
+
     // Create the promise and SES service object
-    var sendPromise = new AWS.SES({apiVersion: '2010-12-01'}).sendEmail(params).promise();
-  
-  // Handle promise's fulfilled/rejected states
-    sendPromise.then(
-      function(data) {  
-        res.status(204).send()  
-      }).catch(
-        function(err) {
-          res.send(err)
+    var sendPromise = new AWS.SES({ apiVersion: "2010-12-01" })
+      .sendEmail(params)
+      .promise();
+
+    // Handle promise's fulfilled/rejected states
+    sendPromise
+      .then(function (data) {
+        res.status(204).send();
+      })
+      .catch(function (err) {
+        res.send(err);
       });
   }
-})
+});
 
-app.post('/camp/grit-camp', function (req, res) {
+app.post("/camp/grit-camp", function (req, res) {
   if (req.body.email) {
     var params = {
-      Destination: { /* required */
+      Destination: {
+        /* required */
         CcAddresses: [
           /* more items */
         ],
         ToAddresses: [
-          "contact@pointavenue.com"
+          "contact@pointavenue.com",
           // "trung.le@truenorth.edu.vn"
           // req.body.email,
-          /* more items */        
-        ]
+          /* more items */
+        ],
       },
-      Message: { /* required */
-        Body: { /* required */
+      Message: {
+        /* required */
+        Body: {
+          /* required */
           Html: {
-           Charset: "UTF-8",
-           Data: `
+            Charset: "UTF-8",
+            Data: `
             <p><b>First Name: </b> ${req.body.firstName}</p>
             <p><b>Last Name: </b> ${req.body.lastName}</p>
             <p><b>Email: </b> ${req.body.email}</p>
             <p><b>Child Name: </b> ${req.body.childName}</p>
             <p><b>Phone Number: </b> ${req.body.phoneNumber}</p>
             <p><b>Student's Grade & School: </b> ${req.body.grade}</p>
-          `
+          `,
           },
           Text: {
-           Charset: "UTF-8",
-           Data: "TEXT_FORMAT_BODY"
-          }
-         },
-         Subject: {
-          Charset: 'UTF-8',
-          Data: 'Register Grit Camp'
-         }
+            Charset: "UTF-8",
+            Data: "TEXT_FORMAT_BODY",
+          },
         },
-      Source: 'contact@pointavenue.com', /* required */
+        Subject: {
+          Charset: "UTF-8",
+          Data: "Register Grit Camp",
+        },
+      },
+      Source: "contact@pointavenue.com" /* required */,
       ReplyToAddresses: [
         /* more items */
       ],
     };
-  
+
     // Create the promise and SES service object
-    var sendPromise = new AWS.SES({apiVersion: '2010-12-01'}).sendEmail(params).promise();
-  
-  // Handle promise's fulfilled/rejected states
-    sendPromise.then(
-      function(data) {  
-        res.status(204).send()  
-      }).catch(
-        function(err) {
-          res.send(err)
+    var sendPromise = new AWS.SES({ apiVersion: "2010-12-01" })
+      .sendEmail(params)
+      .promise();
+
+    // Handle promise's fulfilled/rejected states
+    sendPromise
+      .then(function (data) {
+        res.status(204).send();
+      })
+      .catch(function (err) {
+        res.send(err);
       });
   }
-})
+});
